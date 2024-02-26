29ea325940b2fc049734c0a56434650954fa6413
himu <satirtharoy2003@gmail.com>
2024-02-26 11:45:40 +0600

    mouse follower gsap js upgraded

--- mf using gsap js/index.js
+++ mf using gsap js/index.js
@@ -1,29 +1,30 @@
 const mf = document.querySelector('.mf');
+const txt = document.querySelector('.txt');
 let {x, y} = {x: 0, y: 0}
 
 document.body.addEventListener ('mousemove', (e)=> {
-  mf.style.opacity = 1;
   x = e.clientX;
   y = e.clientY;
-  gsap.to('.mf', {x: `${x}px`, y: `${y}px`, duration: .2})
+  gsap.to('.mf', {x: `${x}px`, y: `${y}px`, scale: 1, duration: .2})
+  gsap.to(txt, {x: `${x}px`, y: `${y}px`, scale: 1, duration: .15})
+
 })
 
 document.body.addEventListener('mouseleave', ()=> {
-  mf.style.opacity = 0;
+  gsap.to([mf,txt], {scale: 0})
 })
 
 const butt = document.querySelector('button');
 
 butt.addEventListener('mouseenter', ()=> {
-  // mf.style.scale = 4;
-  gsap.to('.mf', {height: '100px', width: '100px', innerText: 'click', duration: .2});
-  mf.style.color = 'black';
+  gsap.to('.mf', {height: '100px', width: '100px', duration: .2});
+  gsap.to(txt, {backgroundColor: 'black', innerText: 'click'})
+  mf.style.color = 'white';
 })
 
 butt.addEventListener('mouseleave', ()=> {
-  // console.log(mf.style);
-  // mf.style.scale = 1;
-  gsap.to('.mf', {height: '20px', width: '20px', innerText: '', duration: .2});
+  gsap.to('.mf', {height: '80px', width: '80px', duration: .2});
+  gsap.to(txt, {backgroundColor: 'white', innerText: ''})
 })
 
 butt.addEventListener('click', ()=> {
