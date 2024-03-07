1e053eaec152fa584ac2313b7cc2d93d8d25dc3c
himu <satirtharoy2003@gmail.com>
2024-03-07 19:52:08 +0600

    dlted comments

--- text animation/index.js
+++ text animation/index.js
@@ -1,15 +1,3 @@
-// gsap.set('.contain', {transformOrigin: '50% 50%'})
-// gsap.to('.contain', {duration: '1', y: '50vh', x: '50vw', rotate: '145', scale: '1.2', delay: '2', ease: 'bounce'}
-
-// gsap.from('.contain', {duration: '.5', delay: '1', opacity: '0', scale: '0'});
-
-
-// gsap.from('.circle', {duration: .5, delay: 1, opacity: 0, y: 'random(-200,200)', stagger: .3});
-
-// document.querySelector('button').addEventListener('click', ()=> {
-//   gsap.to('.circle', {duration: .25, opacity: 1, y: 0, stagger: .07});
-// })
-
 gsap.to('.letter', {delay: .5, duration: 1, y: 0, stagger: .05, ease: 'power4.out'});
 gsap.to('.circle', {delay: 3, duration: 1, y: 0, stagger: .05, ease: 'power4.out'});
 
