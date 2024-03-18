4d9bde368cee93e111a5f591c018360fdcd581c6
himu <satirtharoy2003@gmail.com>
2024-03-19 01:34:22 +0600

    scroll trigger

---
+++ scroll trigger/index.js
@@ -0,0 +1,11 @@
+// gsap.to('.ball',
+// { scrollTrigger: {
+//   trigger: '.ball',
+//   start: 'center center',
+//   markers: true,
+//   scrub:1.5,
+//   pin:true,
+//   toggleActions: "restart none restart none"
+// },
+//   scale: '2'
+// })
